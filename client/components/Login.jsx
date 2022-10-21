d41653c3e7310a565f57e7617150c58b36a5ba77
Emily Chu <emma@Emilys-MacBook-Pro.local>
2022-10-21 19:28:44 -0400

    basic redux store functionality

---
+++ client/components/Login.jsx
@@ -0,0 +1,15 @@
+const React = require('react');
+import { render } from 'react-dom';
+import { useState } from 'react';
+
+const Login = (props) => {
+  return (
+    <div>
+      HELLO
+      <input></input>
+      <input></input>
+    </div>
+  );
+};
+
+export default Login;
