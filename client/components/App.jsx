93b547cbc87db36f74d59b4f87b7870ad87c7245
Emily Chu <emma@Emilys-MacBook-Pro.local>
2022-10-22 11:18:51 -0400

    created conditional rendering with drop down box

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,4 +1,4 @@
-const React = require('react');
+import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './Login.jsx';
 import { connect } from 'react-redux';
