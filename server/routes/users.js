902bc28d21b39d4c102da3ac5cd3b4d80fb07c6b
andyz5313 <Your Emailand>
2022-10-22 15:00:57 -0400

    bcrypt backend working

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,14 +2,27 @@ const express = require('express');
 const usersController = require('../controllers/usersController');
 const router = express.Router();
 
-router.get('/', 
-  usersController.getUser,
-  (req, res) => {
-    res.status(200).json(res.locals.user)}
+// router.get('/', 
+//   usersController.getUser,
+//   (req, res) => {
+//     res.status(200).json(res.locals.user)}
+// );
+
+router.get('/',
+    usersController.getUsers,
+  (req, res) => res.status(200).send(res.locals.users)
 );
 
-router.post('/',
-    usersController.addUser,
+router.post('/login', 
+    usersController.verifyUser,
+    (req, res) => { 
+    //res.status(200).json(res.locals.users)
+    res.redirect('/main');
+    }
+);
+
+router.post('/signup',
+    usersController.createUser,
   (req, res) => res.status(200).send("added")
 );
 
