db8ccc7a7fa270427620b9f75180005b9313c85e
Emily Chu <emma@Emilys-MacBook-Pro.local>
2022-10-22 10:52:45 -0400

    updated login and signup

--- client/components/Signup.jsx
+++ client/components/Signup.jsx
@@ -14,14 +14,47 @@ const Signup = (props) => {
     <div id='signup'>
       <h1>Wendy and Emily are the best</h1>
       Signup
-      <input name='username' type='text' placeholder='Username'></input>
-      <input name='password' type='password' placeholder='password'></input>
+      <input
+        id='username'
+        name='username'
+        type='text'
+        placeholder='Username'
+      ></input>
+      <input
+        id='password'
+        name='password'
+        type='password'
+        placeholder='Password'
+      ></input>
+      <input
+        id='confirm-pass'
+        name='confirm password'
+        type='password'
+        placeholder='Confirm Password'
+      ></input>
       <button
         id='signup-button'
         onClick={() => {
-          const user = document.getElementById('username').value;
-          const pass = document.getElementById('password').value;
-          createUser(user, pass);
+          const user = document.getElementById('username');
+          const pass = document.getElementById('password');
+          const confirm = document.getElementById('confirm-pass');
+          if (!user.value || !pass.value || !confirm.value) {
+            if (!user.value) {
+              user.style.borderColor = 'red';
+            }
+            if (!pass.value) {
+              pass.style.borderColor = 'red';
+            }
+            if (!confirm.value) {
+              confirm.style.borderColor = 'red';
+            }
+          } else {
+            if (pass !== confirm.value) {
+              confirm.style.borderColor = 'red';
+            } else {
+              createUser(user.value, pass.value);
+            }
+          }
         }}
       >
         Signup
--- client/components/Transactions.jsx
+++ client/components/Transactions.jsx
@@ -1,15 +1,34 @@
 const React = require('react');
 import { useState } from 'react';
 
-// transactions: [{
-//     date:
-//     item:
-//     amount:
-//     category:
-// }]
+transactions: [
+  {
+    date: '',
+    item: '',
+    amount: '',
+    category: '',
+  },
+];
 
 const Transactions = (props) => {
-  return <div className='transactions'></div>;
+  return (
+    <div className='transactions'>
+      <div>
+        <label htmlFor='date'>Date:</label>
+        <span id='date'>{date}</span>
+      </div>
+      <div>
+        <label htmlFor='item'>Transaction:</label>
+        <span id='item'>{item}</span>
+      </div>
+      <label htmlFor='amount'>Amount:</label>
+      <span id='amount'>{amount}</span>
+      <div>
+        <label htmlFor='category'>Category:</label>
+        <span id='category'>{category}</span>
+      </div>
+    </div>
+  );
 };
 
 export default Transactions;
