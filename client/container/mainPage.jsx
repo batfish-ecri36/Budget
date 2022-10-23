a64a9b2e46f8e0c119548d3d8b054e8feb5f1b02
wzhengg99 <wzhengg99@gmail.com>
2022-10-23 13:47:28 -0400

    working on fetching transactions from the database

--- client/container/mainPage.jsx
+++ client/container/mainPage.jsx
@@ -1,21 +1,10 @@
-const React = require('react');
-import { useState } from 'react';
-import Transactions from '../components/Transactions.jsx';
-import Monthly from '../components/Monthly.jsx'
-import Yearly from '../components/Yearly.jsx'
-import Weekly from '../components/Weekly.jsx'
-
-
-transactions: [
-  {
-    date: '',
-    item: '',
-    amount: '',
-    category: '',
-  },
-];
-
-
+const React = require("react");
+import { useState, useEffect } from "react";
+import Transactions from "../components/Transactions.jsx";
+import Monthly from "../components/Monthly.jsx";
+import Yearly from "../components/Yearly.jsx";
+import Weekly from "../components/Weekly.jsx";
+import axios from "axios";
 
 const MainPage = (props) => {
   const transArray = [];
@@ -24,86 +13,107 @@ const MainPage = (props) => {
       date={transaction.date}
       item={transaction.item}
       amount={transaction.amount}
-      category={transactions.category}
+      category={transaction.category}
     />;
   });
-  const [display, setDisplay] = useState('week');
-  if (display === 'week') {
+
+  const [transactions, setTransactions] = useState([]);
+
+  //how to access the current user in state?
+
+  const getExpenses = async () => {
+    const requestOptions = {
+      method: 'GET',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({})
+    };
+    const response = await fetch("http://localhost:3000/transactions");
+    const data = await response.json();
+    setTransactions(data);
+  };
+  
+
+  useEffect(() => {
+    getExpenses();
+  }, []);
+
+  const [display, setDisplay] = useState("week");
+  if (display === "week") {
     return (
       <div>
         <h1>Peter and Andy are also great!</h1>
         <div>
           Week
-          <label htmlFor='list-type'>
+          <label htmlFor="list-type">
             <select
-              id='list-type'
+              id="list-type"
               onChange={(event) => {
                 setDisplay(event.target.value);
               }}
             >
-              <option value='week'>Week</option>
-              <option value='month'>Month</option>
-              <option value='year'>Year</option>
+              <option value="week">Week</option>
+              <option value="month">Month</option>
+              <option value="year">Year</option>
             </select>
           </label>
-          <div id='trans-display'></div>
+          <div id="trans-display"></div>
           {transArray}
           <Weekly />
         </div>
-        <div id='graph'></div>
+        <div id="graph"></div>
       </div>
     );
-  } else if (display === 'month') {
-    if (display === 'month') {
+  } else if (display === "month") {
+    if (display === "month") {
       return (
         <div>
           <h1>Peter and Andy are also great!</h1>
           <div>
             Month
-            <label htmlFor='list-type'>
+            <label htmlFor="list-type">
               <select
-                id='list-type'
+                id="list-type"
                 onChange={(event) => {
                   setDisplay(event.target.value);
                 }}
               >
-                <option value='week'>Week</option>
-                <option value='month'>Month</option>
-                <option value='year'>Year</option>
+                <option value="week">Week</option>
+                <option value="month">Month</option>
+                <option value="year">Year</option>
               </select>
             </label>
-            <div id='trans-display'></div>
+            <div id="trans-display"></div>
             {transArray}
             <Monthly />
           </div>
-          <div id='graph'></div>
+          <div id="graph"></div>
         </div>
       );
     }
-  } else if (display === 'year') {
+  } else if (display === "year") {
     return (
       <div>
         <h1>Peter and Andy are also great!</h1>
         <div>
           Year
-          <label htmlFor='list-type'>
+          <label htmlFor="list-type">
             <select
-              id='list-type'
+              id="list-type"
               onChange={(event) => {
                 setDisplay(event.target.value);
               }}
             >
-              <option value='week'>Week</option>
-              <option value='month'>Month</option>
-              <option value='year'>Year</option>
+              <option value="week">Week</option>
+              <option value="month">Month</option>
+              <option value="year">Year</option>
             </select>
           </label>
-          <div id='trans-display'></div>
+          <div id="trans-display"></div>
           {transArray}
           <Yearly />
         </div>
-        <div id='graph'></div>
-        <div id='summary'></div>
+        <div id="graph"></div>
+        <div id="summary"></div>
       </div>
     );
   }
